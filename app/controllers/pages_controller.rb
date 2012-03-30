8ae3d1702065dcd3e44d8382de7a9b3e408a6902
Suresh <mvsknitw@gmail.com>
2012-03-30 11:37:46 -0700

    Adding user mmicroposts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
   def home
    @title = "HOME"
+    if signed_in?
+      @micropost = Micropost.new
+      @feed_items = current_user.feed.paginate(:page => params[:page])
+    end
   end
 
   def contact
